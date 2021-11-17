3856f7869488063369aa5e9d27157946c8694529
Paul McGeachie <paulmcgeachie@icloud.com>
2021-11-17 21:53:05 +0000

    added query selector

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const link = document.querySelector("a");
+const backgrd = document.querySelector("body");
+
+link.textContent = "Mozilla Developer Network";
+
+link.href = "https://developer.mozilla.org";
