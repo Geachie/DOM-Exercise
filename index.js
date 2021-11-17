aa41f3a6f9375c9970290ed8e12a1e316daf009f
Paul McGeachie <paulmcgeachie@icloud.com>
2021-11-17 22:10:52 +0000

    Manipulate section with JS

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 "use strict";
-
+// Change link content
 const link = document.querySelector("a");
 const backgrd = document.querySelector("body");
 
 link.textContent = "Mozilla Developer Network";
 
 link.href = "https://developer.mozilla.org";
+
+const sect = document.querySelector("section");
+const para = document.createElement("p");
+para.textContent = "We hope you enjoyed the ride.";
+
+sect.appendChild(para);
